9cb512c7ce763f83197a50e6ef3ce93ad3ba80a4
accjesus <accj1990@gmail.com>
2025-04-09 00:09:22 -0300

    ´# inserir

--- repos/AlocacaoDinamica/AlocacaoDinamica/Fila.cs
+++ repos/AlocacaoDinamica/AlocacaoDinamica/Fila.cs
@@ -12,6 +12,21 @@ namespace AlocacaoDinamica
         }
 
         // Inserir na fila
+        public void Inserir(int valor)
+        {
+            if (primeiro == ultimo)
+            {
+                this.primeiro.valor = valor;
+                this.ultimo = new Celula(-1);
+                this.primeiro.prox = this.ultimo;
+            }
+            else
+            {
+                this.ultimo.valor = valor;
+                this.ultimo.prox = new Celula(-1);
+                this.ultimo = this.ultimo.prox;
+            }
+        }
 
         // Remover na fila
 
