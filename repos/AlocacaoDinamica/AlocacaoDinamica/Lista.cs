e7458fcd3433919e830ffc91fdd2ccd456da053f
accjesus <accj1990@gmail.com>
2025-04-11 12:11:54 -0300

    # lista dinâmica

--- repos/AlocacaoDinamica/AlocacaoDinamica/Lista.cs
+++ repos/AlocacaoDinamica/AlocacaoDinamica/Lista.cs
@@ -1,13 +1,57 @@
-﻿namespace AlocacaoDinamica
+﻿using AlocacaoDinamica.classes;
+
+namespace AlocacaoDinamica
 {
     public class Lista
     {
-        // Inserir no Inicio
-        // Inserir no Fim
-        // Inserir na Posição
-        // Remover no Inicio
-        // Remover no Fim
-        // Remover na Posição
+        public Celula? primeiro;
+        public Celula? ultimo;
+
+        public Lista()
+        {
+            this.primeiro = this.ultimo = null;
+        }
+
+        public void InserirInicio(int valor)
+        {
+            Celula tmp = new Celula(valor);
+            tmp.prox = primeiro;
+            primeiro = tmp;
+        }
+
+        public void InserirFim(int valor)
+        {
+            Celula tmp = new Celula(valor);
+            ultimo.prox = tmp;
+            ultimo = tmp;
+        }
+        public void InserirPosicao(int valor, int pos)
+        {
+
+
+        }
+
+        public void RemoverInicio()
+        {
+
+        }
+        public void RemoverFim()
+        {
+
+        }
+
+        public void RemoverPosicao()
+        {
+
+        }
+
+        public void Mostrar()
+        {
+            for (Celula i = primeiro; i != ultimo && i != null; i = i.prox)
+            {
+                Console.WriteLine(i.valor);
+            }
+        }
         // Mostrar
     }
 }
